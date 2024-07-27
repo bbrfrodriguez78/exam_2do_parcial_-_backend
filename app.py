44ee42690c71b775fe14cbd6377e56f686e3fe50
Ronald Rodriguez Castro <bb.rfrodriguezc@gmail.com>
2024-07-27 09:06:49 -0500

    backend
    backend

--- app.py
+++ app.py
@@ -4,16 +4,19 @@ from models import db
 from routes import bp as ingredients_bp
 from flask_cors import CORS
 
-app = Flask(__name__)
-app.config.from_object(Config)
-CORS(app)
+def crear_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    CORS(app)
 
-db.init_app(app)
+    db.init_app(app)
 
-with app.app_context():
-    db.create_all()
+    with app.app_context():
+        db.create_all()
 
-app.register_blueprint(ingredients_bp, url_prefix='/api')
+    app.register_blueprint(ingredients_bp, url_prefix='/api')
+    return app
 
 if __name__ == '__main__':
+    app = crear_app()
     app.run(port=5001)
